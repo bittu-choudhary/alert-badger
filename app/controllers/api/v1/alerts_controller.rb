7888990ba3eae56aa88b79d601be02a88b174cb4
Bittu Choudhary <bittuchoudhary007@gmail.com>
2023-03-09 03:15:05 +0530

    add basic authentication for alert apis

--- app/controllers/api/v1/alerts_controller.rb
+++ app/controllers/api/v1/alerts_controller.rb
@@ -1,2 +1,12 @@
 class Api::V1::AlertsController < ApplicationController
+    before_action :verify_request, only: %i[ create ]
+
+    private
+
+    def verify_request
+        project_name, request_token = ::Base64.decode64(request.authorization.split(' ', 2).last || '').split(":")
+        @project = Project.find_by(title: project_name)
+        render json: { errors: {"request" => ["Not authenticated"]} }, status: :forbidden if !@project || @project.secret_token != request_token
+    end
+
 end
