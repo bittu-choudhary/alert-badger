aaac4fd45989e02c271eca79fb391c418bb0d67d
Bittu Choudhary <bittuchoudhary007@gmail.com>
2023-03-09 02:56:46 +0530

    add alert detail model

--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,23 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_03_08_212150) do
+ActiveRecord::Schema[7.0].define(version: 2023_03_08_212607) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "citext"
   enable_extension "pgcrypto"
   enable_extension "plpgsql"
+
+  create_table "alert_details", id: :uuid, default: -> { "gen_random_uuid()" }, force: :cascade do |t|
+    t.string "name"
+    t.string "alert_type"
+    t.string "description"
+    t.string "from"
+    t.uuid "project_id", null: false
+    t.jsonb "payload"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["project_id"], name: "index_alert_details_on_project_id"
+  end
 
   create_table "clients", id: :uuid, default: -> { "gen_random_uuid()" }, force: :cascade do |t|
     t.string "name", null: false
@@ -22,6 +34,15 @@ ActiveRecord::Schema[7.0].define(version: 2023_03_08_212150) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["user_id"], name: "index_clients_on_user_id"
+  end
+
+  create_table "projects", id: :uuid, default: -> { "gen_random_uuid()" }, force: :cascade do |t|
+    t.string "title", null: false
+    t.uuid "client_id", null: false
+    t.string "secret_token", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["client_id"], name: "index_projects_on_client_id"
   end
 
   create_table "user_login_change_keys", id: :uuid, default: nil, force: :cascade do |t|
@@ -49,7 +70,9 @@ ActiveRecord::Schema[7.0].define(version: 2023_03_08_212150) do
     t.index ["email"], name: "index_users_on_email", unique: true, where: "(status = ANY (ARRAY[1, 2]))"
   end
 
+  add_foreign_key "alert_details", "projects"
   add_foreign_key "clients", "users"
+  add_foreign_key "projects", "clients"
   add_foreign_key "user_login_change_keys", "users", column: "id"
   add_foreign_key "user_password_reset_keys", "users", column: "id"
   add_foreign_key "user_verification_keys", "users", column: "id"
