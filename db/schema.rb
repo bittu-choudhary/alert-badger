f3e5632f63576c93fd6464a9d49514536b1b86fd
Bittu Choudhary <bittuchoudhary007@gmail.com>
2023-03-09 02:58:32 +0530

    add notification type model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_03_08_212607) do
+ActiveRecord::Schema[7.0].define(version: 2023_03_08_212756) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "citext"
   enable_extension "pgcrypto"
@@ -34,6 +34,13 @@ ActiveRecord::Schema[7.0].define(version: 2023_03_08_212607) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["user_id"], name: "index_clients_on_user_id"
+  end
+
+  create_table "notification_types", id: :uuid, default: -> { "gen_random_uuid()" }, force: :cascade do |t|
+    t.string "name", null: false
+    t.string "description"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "projects", id: :uuid, default: -> { "gen_random_uuid()" }, force: :cascade do |t|
