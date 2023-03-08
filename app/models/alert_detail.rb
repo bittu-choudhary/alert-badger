f663d727e0054ba16d03c4cd355f134acee4e526
Bittu Choudhary <bittuchoudhary007@gmail.com>
2023-03-09 03:26:20 +0530

    add is_spam method

--- app/models/alert_detail.rb
+++ app/models/alert_detail.rb
@@ -1,4 +1,10 @@
 class AlertDetail < ApplicationRecord
   belongs_to :project
   has_one :notification_detail
+
+  def is_spam?
+    spam_detector = SpamDetectionService.new(self)
+    spam_detector.inspect
+  end
+  
 end
