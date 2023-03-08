0ad05b7e383abe0d2f07885ede98111e2fa3a590
Bittu Choudhary <bittuchoudhary007@gmail.com>
2023-03-09 02:50:38 +0530

    Add authentication and authorization methods

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,27 @@
 class ApplicationController < ActionController::API
+    after_action :set_jwt_token
+    
+    include Pundit::Authorization
+
+    rescue_from Pundit::NotAuthorizedError, with: :not_authorized
+
+    def no_matching_route
+        render json: { errors: {"routingError" => ["No route matches"]} }, status: :not_found
+    end
+    
+    private 
+
+    def authenticate
+        rodauth.require_authentication
+    end
+
+    def not_authorized
+        render json: { errors: {"request" => ["not authorized"]} }, status: :forbidden
+    end
+
+    def set_jwt_token
+        if rodauth.use_jwt? && rodauth.valid_jwt?
+            response.headers["Authorization"] = rodauth.session_jwt
+        end
+    end
 end
