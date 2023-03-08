641703e4a0115db4b838d0f81379abc5d6baaf28
Bittu Choudhary <bittuchoudhary007@gmail.com>
2023-03-09 03:15:25 +0530

    Add strong params

--- app/controllers/api/v1/alerts_controller.rb
+++ app/controllers/api/v1/alerts_controller.rb
@@ -9,4 +9,8 @@ class Api::V1::AlertsController < ApplicationController
         render json: { errors: {"request" => ["Not authenticated"]} }, status: :forbidden if !@project || @project.secret_token != request_token
     end
 
+    def alert_params
+        params.require(:alert).permit(:Type, :RecordType, :TypeCode, :Name, :Tag, :MessageStream, :Description, :Email, :From, :BouncedAt)
+    end
+
 end
